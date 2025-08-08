57e2b260afa4666cb4aa4a4ecb4aababdc4128c0
JavierAle <javieragustinale@gmail.com>
2025-08-08 18:21:32 +0200

    Added service

--- src/app/components/home/home.component/home.component.ts
+++ src/app/components/home/home.component/home.component.ts
@@ -1,19 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { Car } from '../../../interfaces/car.interface';
 import { __mockCars } from '../../../__mock/__mockCars';
-import { delay } from 'rxjs';
+import { CarService } from '../../../services/car/car.service';
+import { NEVER, Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
+  providers: [CarService],
   standalone: true,
 })
 export class HomeComponent implements OnInit {
-  public cars: Car[] = [];
+  public carsList$: Observable<Car[]> = NEVER;
+  public loading$: Observable<boolean> = NEVER;
+
+  constructor(private carService: CarService) {}
 
   public ngOnInit(): void {
-    this.cars = __mockCars;
+    this.loading$ = this.carService.loading$;
+    this.carsList$ = this.carService.carList$;
   }
 }
--- src/app/services/car/car.service.ts
+++ src/app/services/car/car.service.ts
@@ -1,8 +1,23 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
+import { delay, NEVER, Observable, of, tap } from 'rxjs';
+import { Car } from '../../interfaces/car.interface';
+import { __mockCars } from '../../__mock/__mockCars';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class CarService {
-  
+  public carList$: Observable<Car[]> = NEVER;
+  public loading$: Observable<boolean> = NEVER;
+
+  constructor() {
+    this.defineStreams();
+  }
+
+  private getCarList(): Observable<Car[]> {
+    this.loading$ = of(true);
+    return of(__mockCars).pipe(delay(1000));
+  }
+
+  private defineStreams(): void {
+    this.carList$ = this.getCarList();
+  }
 }
